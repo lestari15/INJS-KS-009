852853e65b2f5f33babd95b42ff7da3cae01a716
lestari15 <lestari.18411045@student.ubl.ac.id>
2021-10-25 07:58:02 -0700

    assignment3

--- tugas2/__tests__/server.test.js
+++ tugas2/__tests__/server.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('../app.js');
 
-test('GET /', async() => {
+test('GET /', async () => {
     const response = await request(app).get('/getData');
     expect(response.status).toBe(500);
 });
 
-test('POST /', async() => {
+test('POST /', async () => {
     const response = await request(app).post('/login');
     expect(response.status).toBe(200);
 });
